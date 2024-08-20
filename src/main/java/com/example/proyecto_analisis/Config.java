899124da1a43b7258c4e087004b3c46220d90645
Daniel <danyochoa112@gmail.com>
2024-08-19 23:04:03 -0600

    Se agrega front deployed a cors

--- src/main/java/com/example/proyecto_analisis/Config.java
+++ src/main/java/com/example/proyecto_analisis/Config.java
@@ -15,7 +15,7 @@ public class Config implements WebMvcConfigurer{
             @Override 
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","http://107.23.227.110")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
